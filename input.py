22214c82304dff0f06a3ef878eb0c69a8792161f
Chrus <43400126+Chrus3@users.noreply.github.com>
2019-12-02 07:00:16 +1100

    Some changes

--- input.py
+++ input.py
@@ -1,5 +1,8 @@
 import json
 with open('input.json', 'r') as input:
     obj = json.load(input)
-    print('Hello, ' + obj['name'])
+    with open('output.txt', 'w') as output:
+        output.write(obj['name'] +"'s hobbies:\n")
+        for hobby in obj['hobbies']:
+            output.write(hobby + "\n")
     
